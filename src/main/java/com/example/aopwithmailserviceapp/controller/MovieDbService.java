708d3540db0ca848809202a68679ffbda9df4813
Michał Szymanowski <michal.szymanowski88@gmail.com>
2021-09-19 00:05:47 +0200

    Initial Commit

--- src/main/java/com/example/aopwithmailserviceapp/controller/MovieDbService.java
+++ src/main/java/com/example/aopwithmailserviceapp/controller/MovieDbService.java
@@ -14,111 +14,36 @@ import java.util.List;
 @Service
 public class MovieDbService implements MovideDbDao {
 
-public List<RequestResult> listOfMovies ;
+    public List<RequestResult> listOfMovies;
+    public final String key = "9fa5af48946037e925e1a20c951e797d";
 
 
-MovieDbService()
-{
-listOfMovies = new ArrayList<>();
+    MovieDbService() {
+        listOfMovies = new ArrayList<>();
 
-}
+    }
+
     @EventListener(ApplicationReadyEvent.class)
-    public  List<RequestResult> movies()
-    {
+    public List<RequestResult> movies() {
 
         RestTemplate restTemplate = new RestTemplate();
-
-        MovieRequest movieRequest = restTemplate.getForObject("https://api.themoviedb.org/3/movie/popular?api_key=9fa5af48946037e925e1a20c951e797d",MovieRequest.class);
-
-
+        MovieRequest movieRequest = restTemplate.getForObject("https://api.themoviedb.org/3/movie/popular?api_key="+key, MovieRequest.class);
         listOfMovies = movieRequest.getResults();
 
-        for(RequestResult movieRequest1 : listOfMovies)
-        {
-
-            System.out.println(movieRequest1);
-        }
-
-//       for(RequestResult movieRequest1 : movieRequest.getResults())
-//       {
-//           System.out.println(movieRequest1);
-//
-//       }
         return listOfMovies;
 
 
-
     }
 
-
-
-//   public MovieRequest movies()
-//   {
-//
-//       RestTemplate restTemplate = new RestTemplate();
-//
-//       MovieRequest movieRequest = restTemplate.getForObject("https://api.themoviedb.org/3/movie/popular?api_key=9fa5af48946037e925e1a20c951e797d",MovieRequest.class);
-//
-//
-//      listOfMovies = movieRequest.getResults();
-//
-//        for(RequestResult movieRequest1 : listOfMovies)
-//        {
-//
-//            System.out.println(movieRequest1);
-//        }
-//
-////       for(RequestResult movieRequest1 : movieRequest.getResults())
-////       {
-////           System.out.println(movieRequest1);
-////
-////       }
-//       return movieRequest;
-//
-//
-//
-//   }
-
     @Override
-//    @MailAspect
     public boolean addMovie(String title, String release) {
 
-        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
-    RequestResult newMovie = new RequestResult();
-    newMovie.setTitle(title);
-    newMovie.setReleaseDate(release);
+        RequestResult newMovie = new RequestResult();
+        newMovie.setTitle(title);
+        newMovie.setReleaseDate(release);
 
-       return  listOfMovies.add(newMovie);
+        return listOfMovies.add(newMovie);
 
-
-    }
-
-
-    //@EventListener(ApplicationReadyEvent.class)
-//public RequestResult requestResult()
-//{
-//
-//    RestTemplate restTemplate = new RestTemplate();
-//    RequestResult requestResult = restTemplate.getForObject("https://api.themoviedb.org/3/movie/popular?api_key=9fa5af48946037e925e1a20c951e797d",RequestResult.class);
-//
-//    System.out.println("dziala");
-//    System.out.println(requestResult.toString());
-//            return requestResult;
-//
-//}
-
-
-    @Override
-    public String dataRequest() {
-//        RestTemplate restTemplate = new RestTemplate();
-//      RequestResult requestResult = restTemplate.getForObject("https://api.themoviedb.org/3/movie/popular?api_key=9fa5af48946037e925e1a20c951e797d",RequestResult.class);
-//
-//        RestTemplate restTemplate = new RestTemplate();
-//        RequestResult[] requestResult = restTemplate.getForObject("https://api.themoviedb.org/3/movie/popular?api_key=9fa5af48946037e925e1a20c951e797d",RequestResult[].class);
-//
-//return requestResult.toString();
-
-        return null;
 
     }
 }
--- src/main/java/com/example/aopwithmailserviceapp/service/MovideDbDao.java
+++ src/main/java/com/example/aopwithmailserviceapp/service/MovideDbDao.java
@@ -2,8 +2,7 @@ package com.example.aopwithmailserviceapp.service;
 
 public interface MovideDbDao {
 
-    String dataRequest();
-//    @MailAspect
+
     boolean addMovie(String title, String release);
 
 
